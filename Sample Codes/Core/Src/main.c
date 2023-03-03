ab3b646de4a49c50a9efa59de901739cbef19664
cuong <tongcuong89@gmail.com>
2023-03-03 07:10:23 +0700

    Create a sample code

---
+++ Sample Codes/Core/Src/main.c
@@ -0,0 +1,337 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* Private define ------------------------------------------------------------*/
+
+//-------------------------------------------------------------
+// DINH NGHIA CAC LED TREN CHAN PD12, PD13, PD14, PD15
+//------------------------------------------------------------
+#define LED_GREEN	(1U << 12)
+#define LED_ORANGE	(1U << 13)
+#define LED_RED		(1U << 14)
+#define LED_BLUE	(1U << 15)
+
+//-------------------------------------------------------------
+// DINH NGHIA NUT BAM TREN PIN PA0
+//-------------------------------------------------------------
+#define BUTTON		(*((uint32_t*)(0x40020000 + 0x10)) & 1U)
+
+//-------------------------------------------------------------
+// DINH NGHIA CAC THANH GHI PORT D
+//-------------------------------------------------------------
+#define GPIOD_MODER		(*((uint32_t*)(0x40020C00 | 0x00)))	// GPIO port D mode select register
+#define GPIOD_OTYPER	(*((uint32_t*)(0x40020C00 | 0x04)))	// GPIO port D output type register
+#define GPIOD_ODR		(*((uint32_t*)(0x40020C00 | 0x14)))	// GPIO port D output data register
+
+//-------------------------------------------------------------
+// DINH NGHIA CAC THANH GHI PORT A
+//-------------------------------------------------------------
+#define GPIOA_MODER		(*((uint32_t*)(0x40020000 | 0x00)))	// GPIO port A mode select register
+
+// For I/O function
+#define GPIOA_IDR		(*((uint32_t*)(0x40020000 | 0x10)))	// GPIO port A input data register
+#define GPIOA_PUPDR		*((uint32_t*)(0x40020000 | 0x0C))	// GPIO port A pull-up/pull-down register
+
+// For alternate function
+#define GPIOA_AFRL		(*((uint32_t*)(0x40020000 | 0x20)))	// GPIO alternate function low register
+#define CR1				(*((uint32_t*)(0x4000440C)))		//
+#define BRR				(*((uint32_t*)(0x40004408)))		//
+#define DR				(*((uint32_t*)(0x40004404)))		//
+#define SR				(*((uint32_t*)(0x40004400)))		//
+//-------------------------------------------------------------
+// DINH NGHIA CAC THANH NGAT NGOAI
+//-------------------------------------------------------------
+#define EXTI_IMR		(*((uint32_t*)(0x40013C00 | 0x00)))	// Interrupt mask register
+#define EXTI_RTSR		(*((uint32_t*)(0x40013C00 | 0x08)))	// Rising trigger selection register
+#define EXTI_PR			(*((uint32_t*)(0x40013C00 | 0x14)))	// Pending register
+
+
+#define NVIC_ISER0		(*((uint32_t*)(0xE000E100 | 0x00)))	// Interrupt set-enable register 0
+#define VTOR			(*((uint32_t*)(0xE000ED08 | 0x00)))
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+void ledInit(void);
+//void ledCtrl(LED_NUMBER_t ledNumber, LED_STATE_t ledState);
+void buttonInit(void);
+void exti0Init(void);
+void ISR_EXTI0(void);
+void uart2_init(void);
+void uartSend1Byte(uint8_t);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+//-------------------------------------------------------------
+// KHOI TAO PD12, PD13, PD14, PD15 in OUTPUT MODE (push-pull)
+//-------------------------------------------------------------
+void ledInit()
+{
+	__HAL_RCC_GPIOD_CLK_ENABLE();		// Enable clock for GPIOD
+	/*
+	 * to do:
+	 * set PD12, PD13, PD14, PD15 in OUTPUT MODE (push-pull)
+	 */
+	GPIOD_MODER &= ~(0xFF << 24);		// Clear
+	GPIOD_MODER |= (0b01 << 24) | (0b01 << 26) | (0b01 << 28) | (0b01 << 30);
+
+	GPIOD_OTYPER &= ~(0b1111 << 12);
+}
+
+//void ledCtrl(LED_NUMBER_t ledNumber, LED_STATE_t ledState)
+//{
+////	uint32_t *GPIOD_ODR	= (uint32_t*)(0x40020C00 + 0x14);
+//	if(ledState == LED_ON)
+//	{
+//		GPIOD_ODR |= LED_GREEN << ledNumber;
+//	}
+//	else
+//	{
+//		GPIOD_ODR &= ~(LED_GREEN << ledNumber);
+//	}
+//}
+//-------------------------------------------------------------
+// KHOI TAO NUT BAM TREN PIN PA0 (PA0 as INPUT)
+//-------------------------------------------------------------
+
+void buttonInit()
+{
+	__HAL_RCC_GPIOA_CLK_ENABLE();	// Enable clock for GPIOA
+	/*
+	 * to do:
+	 * set PA0 is input (no pull up no pull down)
+	 */
+	GPIOA_MODER &= ~(3U);		// PA0 as input
+	GPIOA_PUPDR &= ~(3U);		// No pull up, no pull down
+}
+
+//-------------------------------------------------------------
+// KHOI TAO NGAT NGOAI 0 (EXTI0)
+//-------------------------------------------------------------
+void exti0Init()
+{
+	EXTI_IMR |= 1U << 0;
+	EXTI_RTSR |= 1U << 0;
+	NVIC_ISER0 |= 1U << 6;
+}
+
+//-------------------------------------------------------------
+// TRINH PHUC VU NGAT NGOAI 0
+//-------------------------------------------------------------
+void ISR_EXTI0()
+{
+//	ledCtrl(LED3, LED_ON);
+//	ledCtrl(LED1, LED_ON);
+	EXTI_PR |= 1U << 0;		// Clear interrupt flag
+}
+
+//-------------------------------------------------------------
+// UART2
+//-------------------------------------------------------------
+void uart2_init()
+{
+	__HAL_RCC_GPIOA_CLK_ENABLE();		// Enable clock for GPIOD
+	GPIOA_MODER |= (2U << 4) | (2U << 6); // PA2, PA3 as Alternate function mode
+	GPIOA_AFRL &= ~(0xFFU << 8);
+	GPIOA_AFRL |= (7U << 8) | (7U << 12);
+
+	__HAL_RCC_USART2_CLK_ENABLE();
+	BRR = (104U << 4) | (3U << 0);
+	CR1 = (1U << 3) | (1U << 2) | (1U << 13);
+}
+
+//-------------------------------------------------------------
+// UART SEND 1 BYTE
+//-------------------------------------------------------------
+void uartSend1Byte(uint8_t data)
+{
+	while (((SR >> 7)&1) != 1);
+	DR = data;
+	while (((SR >> 6)&1) != 1);
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+	//-------------------------------------------------------------
+	// DI CHUYEN BANG VECTOR NGAT (MOVE VECTOR TABLE)
+	//-------------------------------------------------------------
+	// Move vector table to RAM (0x20000000)
+	memcpy(0x20000000, 0x00, 0x198);
+
+	// Set NVIC vector table offset to 0x20000000
+	VTOR = 0x2000000;
+
+	// Register ISR_EXTI0 to EXTI0 interrup event
+	uint32_t *temp = 0x20000058;
+	*temp = (uint32_t)ISR_EXTI0 | 1U;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
